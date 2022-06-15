1cc8833f1125776975f2bd3ca5966d4c7799a77b
lyq <1661898579@qq.com>
2022-06-15 14:09:11 +0800

    加入顶象无感验证

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -10,15 +10,15 @@
             <el-form-item label="管理员密码" prop="password">
               <el-input type="password" v-model="LoginForm.password"></el-input>
             </el-form-item>
-            <!-- <el-form-item>
-              <el-checkbox name="rememberMe" v-model="LoginForm.rememberMe">记住我</el-checkbox>
-            </el-form-item> -->
+            <el-from-item>
+              <div ref="demo" style="margin:0 auto"></div>
+            </el-from-item>
             <el-form-item>
               <v-btn class="loginBtn" @click="login">登录</v-btn>
             </el-form-item>
           </el-form>
         </el-tab-pane>
-        <el-tab-pane label="手机号登录" name="second">
+        <!-- <el-tab-pane label="手机号登录" name="second">
           <el-form :model="LoginPhoneForm"  :rules="PhoneRules" ref="LoginPhoneForm" label-width="100px" class="demo-ruleForm">
             <el-form-item label="管理员账号" prop="phone">
               <el-input v-model="LoginPhoneForm.phone"></el-input>
@@ -31,7 +31,7 @@
               <v-btn class="loginBtn">登录</v-btn>
             </el-form-item>
           </el-form>
-        </el-tab-pane>
+        </el-tab-pane> -->
       </el-tabs>
      
       <div class="main" id="main">
@@ -42,6 +42,7 @@
         <span>N</span>
       </div>
     </div>
+    
   </div>
 </template>
 
@@ -69,12 +70,22 @@ export default {
         phone:[{ required: true, message: '请输入手机号', trigger: 'blur' }],
         code:[{ required: true, message: '请输入手机号', trigger: 'blur' }],
       },
-      activeName:'first'
+      activeName:'first',
+      codetoken:'',
     }
   },
-  mounted(){
-    // const that = this
-    // document.addEventListener('keyup',that.draw())
+  mounted() {
+    _dx.Captcha(this.$refs.demo, {
+      // appId, 在控制台应用管理或应用配置模块获取
+      appId: "40ba323954dfade3e33c299a192faa40",
+      apiServer: 'https://dxvip.dingxiang-inc.com',  
+      // apiServer域名地址在控制台页面->无感验证->应用管页面左上角获取，必须填写完整包括https://。
+      success: token => {
+        // 获取验证码token，用于后端校验，注意获取token若是sl开头的字符串，则是前端网络不通生成的降级token,请检查前端网络及apiServer地址。
+        this.codetoken = token
+        console.log(token);
+      }
+    });
   },
   methods:{
     draw(){
@@ -100,6 +111,9 @@ export default {
     login(){
       this.$refs.LoginForm.validate(async valid=>{
         if(!valid) return
+        if(!this.codetoken){
+          return this.$message.error('请完成验证码验证')
+        }
         const{data:res}=await this.$http.post('admin/oauth/token?'+qs.stringify(this.LoginForm))
         // console.log(this.LoginForm);
         console.log(res);
